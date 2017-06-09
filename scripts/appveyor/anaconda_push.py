195093aeb75def9bcef3014ff3a2f142342488af
Maciek Wójcikowski <maciek@wojcikowski.pl>
2017-06-09 14:41:36 +0200

    Upload to Anaconda.org only from master

--- scripts/appveyor/anaconda_push.py
+++ scripts/appveyor/anaconda_push.py
@@ -4,9 +4,12 @@ import subprocess
 import traceback
 
 token = os.environ['ANACONDA_TOKEN']
+branch = os.environ['APPVEYOR_REPO_BRANCH']
 cmd = ['anaconda', '-t', token, 'upload', '--force']
 cmd.extend(glob.glob('*.tar.bz2'))
-try:
-    subprocess.check_call(cmd)
-except subprocess.CalledProcessError:
-    traceback.print_exc()
+
+if branch == 'master':  # Upload only from master branch
+    try:
+        subprocess.check_call(cmd)
+    except subprocess.CalledProcessError:
+        traceback.print_exc()
