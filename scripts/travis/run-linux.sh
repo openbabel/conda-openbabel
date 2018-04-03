6131602afa18f967af3bdae8d09863d9c1e34cd4
Maciek Wójcikowski <maciek@wojcikowski.pl>
2018-04-03 11:44:44 +0200

    Push from docker

--- scripts/travis/run-linux.sh
+++ scripts/travis/run-linux.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 set -e
-mkdir -p conda-bld
 docker pull condaforge/linux-anvil
 docker run -e CONDA_PY -e ANACONDA_TOKEN -e TRAVIS_BRANCH \
     -v `pwd`/recipe:/recipe \
-    -v `pwd`/conda-bld:/opt/conda/conda-bld \
-    condaforge/linux-anvil /bin/bash -c \
-    "conda config --remove channels conda-forge; conda update --yes conda; conda install --yes conda-build cmake; conda config --add channels openbabel; conda build /recipe;"
-
-ls -lnh conda-bld/*/
-
-# upload only on master
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-    anaconda -t $ANACONDA_TOKEN upload --force conda-bld/*/*.tar.bz2
-fi
+    condaforge/linux-anvil /bin/bash -c "\
+    conda config --remove channels conda-forge && \
+    conda update --yes conda && \
+    conda install --yes conda-build && \
+    conda config --add channels openbabel && \
+    conda build /recipe && \
+    ls -lnh /opt/conda/conda-bld/*/ && \
+    if [ \"\$TRAVIS_BRANCH\" == \"master\" ]; then \
+      anaconda -t \$ANACONDA_TOKEN upload --force /opt/conda/conda-bld/*/*.tar.bz2; \
+    fi;"
